0ff727388ddff5310c4f3c8ea7acf43e24ddb910
east301 <me@east301.net>
2018-09-24 16:57:11 +0900

    reformat codes

--- bench/bench_ldict.py
+++ bench/bench_ldict.py
@@ -1,19 +1,20 @@
-import time
 import sys
-sys.path.append('../dagger')
+import time
+
 import dagger
 
+
 if len(sys.argv) > 1:
-  n = int(sys.argv[1])
+    n = int(sys.argv[1])
 else:
-  n = 100000
+    n = 100000
 
 a = range(n)
 d = dagger.ldict(a)
 
 tic = time.time()
 while d.head:
-  d.remove(d.head.data)
+    d.remove(d.head.data)
 
 toc = time.time()
-print toc-tic
+print(toc - tic)
--- bench/bench_list.py
+++ bench/bench_list.py
@@ -2,18 +2,18 @@ import time
 import sys
 
 if len(sys.argv) > 1:
-  n = int(sys.argv[1])
+    n = int(sys.argv[1])
 else:
-  n = 100000
+    n = 100000
 
 a = range(n)
-d = dict([(v,v) for v in a])
+d = dict([(v, v) for v in a])
 
 tic = time.time()
 while a:
-  v = a[0] 
-  d.pop( v )
-  a.remove(v)
-  
+    v = a[0]
+    d.pop(v)
+    a.remove(v)
+
 toc = time.time()
-print toc-tic
+print(toc - tic)
